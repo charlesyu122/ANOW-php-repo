4a08888d18c6274f4e39f4a91bf6a0e6eabd5401
Charles Yu <charlesyu122@gmail.com>
2012-10-29 14:34:24 +0800

    Added files for db connection

---
+++ db_config.php
@@ -0,0 +1,9 @@
+<? php
+
+/* Database Connection Constants */
+
+define('DB_USER',"root"); // db user
+define('DB_PASSWORD',""); // db password
+define('DB_DATABASE',"anow_db"); // db name
+define('DB_SERVER',"localhost"); // db server
+?>
---
+++ db_connect.php
@@ -0,0 +1,41 @@
+<? php
+
+/* Class to connect to database */
+
+class DB_CONNECT{
+
+    // Constructor
+    function __construct() {
+        // connecting to database
+        $this->connect();
+    }
+
+    // Destructor
+    function __destruct() {
+        // closing db connection
+        $this->close();
+    }
+
+     // Function to connect with database
+    function connect() {
+        // import database connection variables
+        require_once __DIR__ . '/db_config.php';
+
+        // Connecting to mysql database
+        $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
+
+        // Selecting database
+        $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
+
+        // returing connection cursor
+        return $con;
+    }
+
+    // Function to close db connection
+    function close() {
+        // closing db connection
+        mysql_close();
+    }
+
+}
+?>
